fe2a5a25786169f5a44539deccb84903f59a7377
duchnv-icd <huynhnguyenvietduc@gmail.com>
2023-10-19 11:45:01 +0700

    Learn section 5: Sequence types

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log('START');
+const fs = require('fs');
 
-console.log('END');
+const json = fs.readFileSync(__dirname + '/' + 'index.json').toString();
+
+const object = JSON.parse(json);
+const nameObject = JSON.parse(object.name);
+
+console.log('json', json, typeof json);
+console.log('object', object, typeof object);
+console.log('nameObject', nameObject, typeof nameObject);
--- section5-note.py
+++ section5-note.py
@@ -1,5 +1,44 @@
+## List
 listA = list([1,2,3,4])
+listB = [1,2,3,4]
 
-listA
+# This code make error of index out of range
+# listA[4] = 5
 
-print('listA', listA, len(listA))
+# print('listA', listA,listA[-1], len(listA), type(listA))
+
+## Tuple
+tupleA = tuple([1, 'a', True, [2,3]])
+tupleB = (1,'a', True, [2,3])
+tupleC = 1, 'a', True, [2,3]
+
+tupleAA = tupleA[-1]
+
+tupleAA[-1] = 5
+
+a = 10
+b =20
+tupleD = a, b, a+b
+
+listOfTupleB = list(tupleB)
+listOfTupleB[-1][-1] = 9
+
+list2OfTupleB = list(tupleB)
+list2OfTupleB[0] = 0
+
+# print('tupleAA', tupleAA, type(tupleAA))
+# print('tupleA', tupleA, type(tupleA))
+# print('tupleB', tupleB, type(tupleB))
+# print('listOfTupleB', listOfTupleB, type(listOfTupleB))
+# print('list2OfTupleB', list2OfTupleB, type(list2OfTupleB))
+# print('tupleC', tupleC, type(tupleC))
+print('tupleD', tupleD, type(tupleD))
+
+## String
+strA = 'Python\'best features'
+strOfTupleD = str(tupleD)
+tupleOfStrOfTupleD = tuple(strOfTupleD)
+
+# print('strA', strA, type(strA))
+print('strOfTupleD', strOfTupleD, type(strOfTupleD))
+print('tupleOfStrOfTupleD', tupleOfStrOfTupleD, type(tupleOfStrOfTupleD))
