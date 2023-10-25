058a4f9f8a0ccb850a42125acea116e27af44e00
duc-coder <huynhnguyenvietduc@gmail.com>
2023-10-25 22:31:09 +0700

    Update section5-exercises.py

--- section5-exercises.py
+++ section5-exercises.py
@@ -14,4 +14,19 @@ t1 = 1, 2, 3, 4, 5, 6
 t2 = 7, 8, 9, 10
 t3 = 11, 12, 13, 14, 15, 16, 17
 
-# t0 = (t1)
+t0 = []
+t0.extend(t1)
+t0.extend(t2)
+t0.extend(t3)
+
+for i in range(0, len(t0),2):
+  t0[i] = 0
+
+# print('t0', t0)
+
+t00 = []
+t00.extend(t1[1::2])
+t00.extend(t2[1::2])
+t00.extend(t3[1::2])
+
+print('t00', t00)
