79e8880c6bad1995c2f26a16c5693b67fe8aadfb
duchnv-vn <huynhnguyenvietduc@gmail.com>
2023-11-14 19:28:54 +0700

    Do exercise section 14: sort, max, min, round, zip function

--- section14-python-built-in-functions/answers.py
+++ section14-python-built-in-functions/answers.py
@@ -2,7 +2,8 @@
 widgets = [f'w{i}' for i in range(1, 21)]
 skus = [f'sku{i}' for i in range(1, len(widgets) + 1)]
 
-zipped_data = {index: value for (index, value) in zip(widgets, skus)}
+# zipped_data = {widget: sku for (widget, sku) in zip(widgets, skus)}
+zipped_data = dict(zip(widgets, skus))
 
 # print(f'zipped_data: {zipped_data}')
 ## ------------------------------------------
@@ -10,28 +11,43 @@ zipped_data = {index: value for (index, value) in zip(widgets, skus)}
 suits = 'shdc'  # Spades, Hearts, Diamonds, Clubs
 ranks = list('23456789') + ['10', 'J', 'Q', 'K', 'A']
 
-list_of_card = [
-  (number, letter)
-  for letter in suits
-    for number in ranks
-]
+# Solution 1
+def generate_card_dict(*, suits_arg, ranks_arg):
+  output = [
+    (number, letter)
+    for letter in suits_arg
+      for number in ranks_arg
+  ]
+  return output
 
-total_cards = 52
-suits_2 = sorted(suits * round(52/len(suits)), reverse=True)
-ranks_2 = ranks * round(52/len(ranks))
-
-list_of_card_2 = list(
-  list(
-    zip(ranks_2[(len(ranks)*i):(len(ranks)*(i+1))], suits_2))
-      for i in range(len(suits)
-  )
-)
+list_of_card = generate_card_dict(suits_arg=suits, ranks_arg=ranks)
 
 # print(f'list_of_card: {list_of_card}') zip(ranks_2,suits_2)
 # print(f'length of list_of_card: {len(list_of_card)}')
 
-# print(f'ranks_2: {ranks_2}')
-print(f'list_of_card_2: {list_of_card_2}')
+# Solution 2
+def generate_card_dict_2(*, suits_arg, ranks_arg):
+  output = [
+    list(zip(ranks_arg, suit * len(ranks_arg)))
+    for suit in suits_arg
+  ]
+  return output
+
+list_of_card_2 = generate_card_dict_2(suits_arg=suits, ranks_arg=ranks)
+
+# print(f'list_of_card_2: {list_of_card_2}')
 
 ## ------------------------------------------
 ## QUESTION 3
+def func_3(numbers, *, reverse=False):
+  sorted_numbers = sorted(numbers, reverse=reverse)
+  min_number = min(numbers)
+  max_number = max(numbers)
+
+  return sorted_numbers, min_number, max_number
+
+sorted_numbers, min_number, max_number = func_3([7,5,8,3,5,2,4,1], reverse=True)
+
+# print(f'sorted_numbers: {sorted_numbers}')
+# print(f'min_number: {min_number}')
+# print(f'max_number: {max_number}')
