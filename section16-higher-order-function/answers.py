aff9cc839a0841ae435d55ff985ca8e10016dbac
duchnv-vn <huynhnguyenvietduc@gmail.com>
2023-11-22 10:49:40 +0700

    Do exercise section 16: Higher order functions

--- section16-higher-order-function/answers.py
+++ section16-higher-order-function/answers.py
@@ -45,11 +45,11 @@ def comprehension_func(points, f):
 def map_func(points, f):
     return map(f, points)
 
-import_points = import_list_func(points)
+import_points = import_list_func(points * 1_000_000)
 
-result_1 = import_points(for_func, handle_each)
-result_2 = import_points(comprehension_func, handle_each)
-result_3 = import_points(map_func, handle_each)
+# result_1 = import_points(for_func, handle_each)
+# result_2 = import_points(comprehension_func, handle_each)
+# result_3 = import_points(map_func, handle_each)
 
 # print(f'result_1: {result_1}')
 # print(f'result_2: {result_2}')
@@ -57,6 +57,42 @@ result_3 = import_points(map_func, handle_each)
 
 ## ------------------------------------------
 ## QUESTION 3
+def partial_func(f, *args, **kwargs):
+    def inner(a):
+        return f(a, *args, **kwargs)
 
+    return inner
+
+def power(a, n):
+    return a ** n
+
+# squares = partial_func(power, 2)
+# result_squares = squares(3)
+
+# cubes = partial_func(power, 3)
+# result_cubes = cubes(3)
+
+# print(f'result_squares: {result_squares}')
+# print(f'result_cubes: {result_cubes}')
 ## ------------------------------------------
 ## QUESTION 4
+def log_time(f):
+    def inner(*args, **kwargs):
+        start = perf_counter()
+        result = f(*args, **kwargs)
+        end = perf_counter()
+        print(f'elapsed time: {end - start}')
+        return result
+    return inner
+
+def norm(x, y):
+    return math.sqrt(x**2 + y**2)
+
+def find_index_min(seq):
+    min_ = min(seq)
+    return seq.index(min_)
+
+# norm_logged = log_time(norm)
+# norm_result = norm_logged(1, 1)
+
+# print(f'norm_result: {norm_result}')
