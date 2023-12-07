e5e08dccd391142f2a2087574876f5da6daeb1bb
duchnv-vn <huynhnguyenvietduc@gmail.com>
2023-12-07 11:46:12 +0700

    Learn section 23: Random module

--- section23-random-module/note.py
+++ section23-random-module/note.py
@@ -2,7 +2,17 @@ import random
 
 ## RANDOM SEED
 
-random.seed(1)
-result = random.random()
+# random.seed(1)
+# result = random.random()
 
-print(result)
+# random.seed(0)
+# for _ in range(5):
+#     a = random.randrange(1, 6)
+#     print(a)
+
+data_l = [
+    (1, 12.3),
+    (2, 30.7),
+    (3, 20.5),
+    (4, 36.5),
+]
