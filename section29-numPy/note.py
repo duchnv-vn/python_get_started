fb0d4eaece175c4ff933cb2d06def9e18a25a658
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-01-02 22:20:58 +0700

    29 Numpy: Reshape & stack arrays

--- section29-numPy/note.py
+++ section29-numPy/note.py
@@ -8,7 +8,47 @@ l = [
 
 m = np.array(l)
 
-print(m)
-print(type(m))
-print(m.dtype)
-print(m.shape)
+# print(m)
+# print(type(m))
+# print(m.dtype)
+# print(m.shape)
+
+l_2 = np.zeros((3, 3), dtype=float)
+
+# print(l_2)
+
+l_3 = np.linspace(2, 20, 10, dtype=int)
+l_4 = np.linspace(2, 20, num=10)
+
+# print(l_3)
+# print(l_4)
+
+np.random.seed(123)
+r_1 = np.random.random((3, 4))
+r_2 = np.random.random(5)
+
+# print(r_1)
+# print(r_2)
+
+# === RESHAPE ARRAY
+init_arr_1 = np.arange(12)
+reshaped_arr_1 = np.reshape(init_arr_1, (3, 4))
+# OR: init_arr_1.reshape((3, 4))
+
+# print(f'init_arr_1: {init_arr_1} - shape: {init_arr_1.shape}')
+# print(f'reshaped_arr_1: {reshaped_arr_1}- shape: {reshaped_arr_1.shape}')
+
+# === STACKING ARRAY
+stack_arr_1 = np.arange(10).reshape((2, 5))
+stack_arr_2 = np.arange(10, 20).reshape((2, 5))
+
+stack_arr_3 = np.array([1.9, 2.2, 3.6], dtype=np.float64)
+
+v_stack = np.vstack((stack_arr_1, stack_arr_2))
+h_stack = np.hstack((stack_arr_1, stack_arr_2))
+
+# print(f'stack_arr_1: {stack_arr_1}')
+# print(f'stack_arr_2: {stack_arr_2}')
+# print(f'v_stack: {v_stack}')
+# print(f'h_stack: {h_stack}')
+print(f'stack_arr_3: {stack_arr_3.astype(np.uint16)}')
